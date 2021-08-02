91175df062813a8cdaf4fec49ae8da460f9fe7f9
Sarah Zheng <z.sarah@outlook.com>
2021-08-01 20:14:46 -0400

    added dark mode

--- Prework/Prework/SettingsViewController.swift
+++ Prework/Prework/SettingsViewController.swift
@@ -8,49 +8,23 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var mySwitch: UISwitch!
     
-//    lazy var darkModeSwitch: UISwitch = {
-//        let darkModeSwitch = UISwitch()
-//        darkModeSwitch.tintColor = .green
-//        darkModeSwitch.onTintColor = .green
-//        darkModeSwitch.backgroundColor = .black
-//        return darkModeSwitch
-//    }()
-//    @IBOutlet var mySwitch: UISwitch!
-    
+    @IBAction func darkModeSwitch(_ sender: Any) {
+        setDarkMode()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        view.backgroundColor = .white
-//        view.addSubview(darkModeSwitch)
+        setDarkMode()
     }
     
-//    @IBAction func setDarkMode(_ sender: UISwitch) {
-//        if sender.isOn {
-//            view.backgroundColor = .red
-//        } else {
-//            view.backgroundColor = .blue
-//        }
-//    }
-//
-    @IBAction func setDarkMode() {
-        guard let vc = storyboard?.instantiateViewController(identifier: "darkModeVC") as? DarkModeViewController else {
-            return
+    func setDarkMode() {
+        if mySwitch.isOn {
+            view.backgroundColor = .black
+        } else {
+            view.backgroundColor = .white
         }
-        present(vc, animated: true)
     }
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//        if segue.identifier == "SettingsViewController",
-//           let destVC = segue.destination as? SettingsViewController {
-//            setDarkMode(destVC)}
-//    }
-
 
 }
