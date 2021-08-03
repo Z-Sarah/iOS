e05c70c88dea0e05c6da6a42c556a39e4b5c45b5
Z-Sarah <53502468+Z-Sarah@users.noreply.github.com>
2021-08-03 13:51:54 -0400

    Add features

--- Prework/Prework/SettingsViewController.swift
+++ Prework/Prework/SettingsViewController.swift
@@ -8,33 +8,78 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var mySwitch: UISwitch!
+    @IBOutlet weak var setDefaultTips: UILabel!
+    @IBOutlet weak var tip1: UILabel!
+    @IBOutlet weak var tip2: UILabel!
+    @IBOutlet weak var tip3: UILabel!
+    @IBOutlet weak var darkMode: UILabel!
+
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var field2: UITextField!
+    @IBOutlet weak var field3: UITextField!
+    @IBAction func darkModeSwitch(_ sender: Any) {
+        setDarkMode()
+    }
     
-    var darkModeSwitch: UISwitch!
+    let myKeys = ["tip1","tip2","tip3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDarkMode()
+        setupToolbar()
+        for key in myKeys {
+            let myTip = UserDefaults.standard.string(forKey: key)
+            if key.compare("tip1").rawValue == 0 {
+                field1.text = myTip
+            } else if key.compare("tip2").rawValue == 0 {
+                field2.text = myTip
+            } else {
+                field3.text = myTip
+            }
+        }
     }
     
-    @IBAction func setDarkMode(_ sender: UISwitch) {
-        if sender.isOn {
-            sender.setOn(false, animated: true)
+    func setDarkMode() {
+        if mySwitch.isOn {
             view.backgroundColor = .black
+            setDefaultTips.textColor = .white
+            tip1.textColor = .white
+            tip2.textColor = .white
+            tip3.textColor = .white
+            darkMode.textColor = .white
         } else {
-            sender.setOn(true, animated: true)
             view.backgroundColor = .white
+            setDefaultTips.textColor = .black
+            tip1.textColor = .black
+            tip2.textColor = .black
+            tip3.textColor = .black
+            darkMode.textColor = .black
         }
+    }
+    
+    @IBAction func setTips(_ sender: Any) {
+        UserDefaults.standard.set(mySwitch.isOn, forKey: "switchMode")
+        NotificationCenter.default.post(name: Notification.Name("switchMode"), object: nil)
         
+        NotificationCenter.default.post(name: Notification.Name("customTip1"), object: field1.text)
+        NotificationCenter.default.post(name: Notification.Name("customTip2"), object: field2.text)
+        NotificationCenter.default.post(name: Notification.Name("customTip3"), object: field3.text)
     }
 
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        
+    func setupToolbar() {
+        let bar = UIToolbar()
+        let doneBotton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissKeyboard))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        bar.items = [flexSpace, flexSpace, doneBotton]
+        bar.sizeToFit()
+        field1.inputAccessoryView = bar
+        field2.inputAccessoryView = bar
+        field3.inputAccessoryView = bar
     }
-
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -14,29 +14,91 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var Settings: UIBarButtonItem!
+    @IBOutlet weak var text1: UILabel!
+    @IBOutlet weak var text2: UILabel!
+    @IBOutlet weak var text3: UILabel!
+    var customTips = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // Sets the title in the Navigation Bar
         self.title = "Tip Calculator"
-    }
 
-    @IBAction func calculateTip(_ sender: Any) {
-        // Get bill amount from text field input
-        let bill = Double(billAmountTextField.text!) ?? 0
+        NotificationCenter.default.addObserver(self, selector: #selector(getNotification(_:)), name: Notification.Name("switchMode"), object: nil)
         
-        // Get Total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        NotificationCenter.default.addObserver(self, selector: #selector(setCustomTip1(_:)), name: Notification.Name("customTip1"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(setCustomTip2(_:)), name: Notification.Name("customTip2"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(setCustomTip3(_:)), name: Notification.Name("customTip3"), object: nil)
+        
+        tipControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.blue], for: .selected)
+    }
+    
+    @objc func setCustomTip1(_ notification: Notification) {
+        var customTip1 = notification.object as! String?
+        tipControl.setTitle(customTip1, forSegmentAt: 0)
+        UserDefaults.standard.setValue(customTip1, forKey: "tip1")
+        if customTip1?.last == "%" {
+            customTip1!.removeLast()
+        }
+        customTips.append((Double(customTip1!)!/100.0))
+    }
+    
+    @objc func setCustomTip2(_ notification: Notification) {
+        var customTip2 = notification.object as! String?
+        tipControl.setTitle(customTip2, forSegmentAt: 1)
+        UserDefaults.standard.setValue(customTip2, forKey: "tip2")
+        if customTip2?.last == "%" {
+            customTip2!.removeLast()
+        }
+        customTips.append((Double(customTip2!)!/100.0))
+    }
+    
+    @objc func setCustomTip3(_ notification: Notification) {
+        var customTip3 = notification.object as! String?
+        tipControl.setTitle(customTip3, forSegmentAt: 2)
+        UserDefaults.standard.setValue(customTip3, forKey: "tip3")
+        if customTip3?.last == "%" {
+            customTip3!.removeLast()
+        }
+        customTips.append((Double(customTip3!)!/100.0))
+    }
+    
+    @objc func getNotification(_ notification: Notification) {
+        let isDarkMode = UserDefaults.standard.bool(forKey: "switchMode")
+        if(isDarkMode) {
+            view.backgroundColor = .black
+            tipAmountLabel.textColor = .white
+            totalLabel.textColor = .white
+            text1.textColor = .white
+            text2.textColor = .white
+            text3.textColor = .white
+        } else {
+            view.backgroundColor = .white
+            tipAmountLabel.textColor = .black
+            totalLabel.textColor = .black
+            billAmountTextField.textColor = .black
+            text1.textColor = .black
+            text2.textColor = .black
+            text3.textColor = .black
+        }
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        var tipPercentages = [Double]()
+        let bill = Double(billAmountTextField.text!) ?? 0
+        print(customTips)
+        if !customTips.isEmpty {
+            tipPercentages = [customTips[0],customTips[1],customTips[2]]
+        } else {
+            tipPercentages = [0.15, 0.18, 0.2]
+        }
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
-        
-        // Update Tip Amount Label
+
         tipAmountLabel.text = String(format: "$%.2f", tip)
-        // Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
-        
     }
 
 }
