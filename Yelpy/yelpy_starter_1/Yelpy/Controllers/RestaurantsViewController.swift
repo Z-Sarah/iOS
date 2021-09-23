4ad10e28ae31b162cc1ff64d40e97d8449494516
Sarah Zheng <z.sarah@outlook.com>
2021-09-23 16:38:20 -0400

    added lab 2 feature

---
+++ Yelpy/yelpy_starter_1/Yelpy/Controllers/RestaurantsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class RestaurantsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
+    // ––––– TODO: Next, place TableView outlet here
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // –––––– TODO: Initialize restaurantsArray
+    var restaurantsArray: [Restaurant] = []
+    
+    // ––––– TODO: Add tableView datasource + delegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        getAPIData()
+        tableView.rowHeight = 150
+    }
+    
+    
+    // ––––– TODO: Get data from API helper and retrieve restaurants
+    func getAPIData() {
+        API.getRestaurants() { (restaurants) in
+            guard let restaurants = restaurants else {
+                return
+            }
+            self.restaurantsArray = restaurants
+            self.tableView.reloadData() // reload data!
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create restaurant cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
+        
+        let restaurant = restaurantsArray[indexPath.row]
+        
+        cell.r = restaurant
+        
+        return cell
+    }
+
+// ––––– TODO: Create tableView Extension and TableView Functionality
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let r = restaurantsArray[indexPath.row]
+            let detailViewController = segue.destination as! RestaurantDetailViewController
+            detailViewController.r = r
+        }
+    }
+
+}
