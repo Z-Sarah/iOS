e1071c8453a5ce75a117387b753b796b9d6a4803
Sarah Zheng <z.sarah@outlook.com>
2021-10-06 17:33:31 -0400

    Parstagram Part1

---
+++ Parstagram/Parstagram/FeedViewController.swift
@@ -0,0 +1,78 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Sarah Zheng on 10/4/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
+    let myRefreshControl = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        myRefreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        self.tableView.refreshControl = myRefreshControl
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc func onRefresh() {
+        refresh()
+        
+    }
+
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
+    }
+    
+    func refresh() {
+        run(after: 2) {
+           self.myRefreshControl.endRefreshing()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
+    
+}
