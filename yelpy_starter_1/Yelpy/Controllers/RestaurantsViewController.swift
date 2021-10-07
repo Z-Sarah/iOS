fc66a0aeeaad376d4d7a0591038464791fec4c7e
Sarah Zheng <z.sarah@outlook.com>
2021-10-07 16:49:02 -0400

    add SkeletonView

--- yelpy_starter_1/Yelpy/Controllers/RestaurantsViewController.swift
+++ yelpy_starter_1/Yelpy/Controllers/RestaurantsViewController.swift
@@ -11,11 +11,13 @@ import AlamofireImage
 import Lottie
 import SkeletonView
 
-class RestaurantsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RestaurantsViewController: UIViewController {
     
     // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
     // ––––– TODO: Next, place TableView outlet here
     var animationView: AnimationView?
+    var refresh = true
+    let yelpRefresh = UIRefreshControl()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,60 +27,44 @@ class RestaurantsViewController: UIViewController, UITableViewDataSource, UITabl
     // ––––– TODO: Add tableView datasource + delegate
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        animationView = .init(name: "4762-food-carousel")
-        animationView?.frame = view.bounds
-        animationView?.play()
+               
+        tableView.visibleCells.forEach { $0.showSkeleton() }
         
         tableView.delegate = self
         tableView.dataSource = self
         getAPIData()
         tableView.rowHeight = 150
         
+        yelpRefresh.addTarget(self, action: #selector(getAPIData), for: .valueChanged)
+        tableView.refreshControl = yelpRefresh
+        
         startAnimations()
-//        stopAnimations()
     }
     
-    
     // ––––– TODO: Get data from API helper and retrieve restaurants
-    func getAPIData() {
+    @objc func getAPIData() {
         API.getRestaurants() { (restaurants) in
             guard let restaurants = restaurants else {
                 return
             }
             self.restaurantsArray = restaurants
             self.tableView.reloadData() // reload data!
+            
+            self.yelpRefresh.endRefreshing()
         }
     }
+}
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantsArray.count
-    }
+extension RestaurantsViewController: SkeletonTableViewDataSource {
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // create restaurant cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
-        
-        let restaurant = restaurantsArray[indexPath.row]
-        
-        cell.r = restaurant
-        
-        return cell
-    }
-
-// ––––– TODO: Create tableView Extension and TableView Functionality
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let cell = sender as! UITableViewCell
-        if let indexPath = tableView.indexPath(for: cell) {
-            let r = restaurantsArray[indexPath.row]
-            let detailViewController = segue.destination as! RestaurantDetailViewController
-            detailViewController.r = r
-        }
+    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        return "RestaurantCell"
     }
     
     func startAnimations() {
+        view.isSkeletonable = true
         animationView = .init(name: "4762-food-carousel")
-        animationView!.frame = CGRect(x: view.frame.width / 3, y: view.frame.height / 3, width: 100, height: 100)
+        animationView!.frame = CGRect(x: view.frame.width / 3, y: 156, width: 100, height: 100)
         animationView!.contentMode = .scaleAspectFit
         view.addSubview(animationView!)
         
@@ -90,6 +76,43 @@ class RestaurantsViewController: UIViewController, UITableViewDataSource, UITabl
     @objc func stopAnimations() {
         animationView?.stop()
         view.subviews.last?.removeFromSuperview()
+        view.hideSkeleton()
+        refresh = false
+    }
+}
+    
+   
+// ––––– TableView Functionality –––––
+extension RestaurantsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create restaurant cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
+        
+        cell.r = restaurantsArray[indexPath.row]
+        
+        cell.showSkeleton()
+        // Stop animation after like .5 seconds
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (timer) in
+            cell.stopSkeletonAnimation()
+            cell.hideSkeleton()
+        }
+        
+        return cell
     }
 
+    // ––––– TODO: Create tableView Extension and TableView Functionality
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let r = restaurantsArray[indexPath.row]
+            let detailViewController = segue.destination as! RestaurantDetailViewController
+            detailViewController.r = r
+        }
+    }
+    
 }
